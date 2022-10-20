e2bc9cc6f9efc8261e2df7afa6f4beb84e51e00a
Volodymyr Pavlov <shark32@students.zcu.cz>
2022-10-20 19:28:15 +0200

    Adding equalsTo() and hashCode() methods

--- src/helpers/Path.java
+++ src/helpers/Path.java
@@ -17,6 +17,8 @@ public class Path implements Comparable<Path> {
      */
     private final double distance;
 
+    private double maxTransitionDistance = -1;
+
     /**
      * Oasis id (destination point)
      */
@@ -35,7 +37,7 @@ public class Path implements Comparable<Path> {
      * @param warehouse_id - id of warehouse (starting point)
      */
     public Path(List<Point> points, int oasis_id, int warehouse_id) {
-        this.points = points;
+        this.points = new ArrayList<>(points);
         this.oasis_id = oasis_id;
         this.warehouse_id = warehouse_id;
         this.distance = calculateDistance();
@@ -48,10 +50,13 @@ public class Path implements Comparable<Path> {
      */
     private double calculateDistance() {
         double distance = 0;
+        double transition;
         for (int i = 0; i < points.size() - 1; ++i) {
             Point p1 = points.get(i);
             Point p2 = points.get(i + 1);
-            distance += Point.getDistance(p1, p2);
+            transition =Point.getDistance(p1, p2);
+            distance += transition;
+            maxTransitionDistance  = Math.max(maxTransitionDistance, transition);
         }
 
         return distance;
@@ -59,7 +64,7 @@ public class Path implements Comparable<Path> {
 
     @Override
     public String toString() {
-        return String.format("Path [%f]: (%d --> %d) %s", distance,warehouse_id + 1,oasis_id + 1,points);
+        return String.format("Path [%f  |  %f]: (%d --> %d) %s", distance,maxTransitionDistance,warehouse_id + 1,oasis_id + 1,points);
         //return "Path []: " + (warehouse_id + 1) + " --> " + (oasis_id + 1) + " " + points;
     }
 
@@ -68,5 +73,18 @@ public class Path implements Comparable<Path> {
         double diff = this.distance - o.distance;
         if (diff == 0.0) return 0;
         return diff < 0 ? -1 : 1;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Path) {
+            return this.points.equals(((Path)obj).points) && this.oasis_id == ((Path)obj).oasis_id && this.warehouse_id == ((Path)obj).warehouse_id;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return points.hashCode();
     }
 }

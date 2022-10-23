9944bc8e3285f38527a60c8d150c8058147d4e16
Volodymyr Pavlov <shark32@students.zcu.cz>
2022-10-23 09:32:23 +0200

    Adding output to the default console. Adding logAll().

--- src/model/EventLogger.java
+++ src/model/EventLogger.java
@@ -1,31 +1,55 @@
 package model;
 
+import console.LogType;
+import console.Logger;
+
 import java.util.PriorityQueue;
 
 public class EventLogger {
 
-    private final PriorityQueue<Event> events = new PriorityQueue<Event>();
+    private final PriorityQueue<Event> events = new PriorityQueue<>();
 
-    public EventLogger() {
+    private final Logger logger;
+
+    public EventLogger(Logger logger) {
+        this.logger = logger;
     }
 
-    public void addEvent(long time, String message) throws IllegalStateException{
+    public void addEvent(double time, String message) throws IllegalStateException {
         events.add(new Event(time, message));
     }
 
-    public void log(long time){
+    public void log(double time) {
         Event event;
-        for(;;){
+        for (; ; ) {
             event = events.peek();
-            if(event == null){
+            if (event == null) {
                 break;
             }
-            if (event.getTime()<=time){
+            if (event.getTime() <= time) {
                 events.poll();
-                System.out.println(event.getMessage());
-            }else{
+                logger.log(event.getMessage(), LogType.EVENT);
+                //System.out.println(event.getMessage());
+            } else {
                 break;
             }
+        }
+    }
+
+    public void print() {
+        Event event;
+        while (!events.isEmpty()) {
+            event = events.poll();
+            System.out.println(event);
+        }
+    }
+
+    public void logAll() {
+        Event event;
+        while (!events.isEmpty()) {
+            event = events.poll();
+            logger.log(event.getMessage(), LogType.EVENT);
+            //System.out.println(event.getMessage());
         }
     }
 }

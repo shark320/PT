f2e669690e21a93030a5521c835549b7c7832262
Volodymyr Pavlov <shark32@students.zcu.cz>
2022-10-25 13:55:57 +0200

    Adding warehouse supply comparable class.

---
+++ src/helpers/WarehouseSupply.java
@@ -0,0 +1,26 @@
+package helpers;
+
+import model.Warehouse;
+
+/**
+ * Record using for warehouses supply queue
+ *
+ * @param warehouse warehouse to supply
+ * @author vpavlov
+ */
+public record WarehouseSupply(Warehouse warehouse) implements Comparable<WarehouseSupply> {
+
+    /**
+     * Getting the warehouse next supply time
+     *
+     * @return the warehouse next supply time
+     */
+    public double getNextSupplyTime() {
+        return warehouse.getNextSupply();
+    }
+
+    @Override
+    public int compareTo(WarehouseSupply o) {
+        return Double.compare(this.warehouse.getNextSupply(), o.warehouse.getNextSupply());
+    }
+}

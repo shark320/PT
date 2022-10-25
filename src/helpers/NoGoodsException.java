4d3ac954a23d2ca7339ebd585458db9b991fdf0c
Volodymyr Pavlov <shark32@students.zcu.cz>
2022-10-25 13:51:11 +0200

    Adding exception for the lack of goods in warehouses

---
+++ src/helpers/NoGoodsException.java
@@ -0,0 +1,15 @@
+package helpers;
+
+/**
+ * Exception for setup when all warehouses do not have enough goods for the request
+ *
+ * @author vpavlov
+ */
+public class NoGoodsException extends Exception{
+
+    /**
+     * Constructor
+     */
+    public NoGoodsException() {
+    }
+}

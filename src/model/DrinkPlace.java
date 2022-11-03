0d74378698cc01d9189d2388ed3ebb24ae52a8a2
Volodymyr Pavlov <shark32@students.zcu.cz>
2022-11-03 12:15:21 +0100

    Initial DrinkPlace

---
+++ src/model/DrinkPlace.java
@@ -0,0 +1,19 @@
+package model;
+
+/**
+ * Class represents a camel drink place
+ *
+ * @author vpavlov
+ */
+public enum DrinkPlace {
+
+    /**
+     * Oasis drink place
+     */
+    OASIS,
+
+    /**
+     * Warehouse drink place
+     */
+    WAREHOUSE
+}
